3ad4ad4b41e9dc5fb8dda676a08ca01d65987a1f
ODentist <o.dentistlsr@gmail.com>
2022-07-09 12:59:57 +0800

    2002/7/9

--- javaSE/src/main/java/day01_Operator.java
+++ javaSE/src/main/java/day01_Operator.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Scanner;
+
 
 public class day01_Operator {
     @Test
@@ -11,6 +13,38 @@ public void Test(){
         System.out.println(p);
         System.out.println(rs);
     }
+@Test
+public void test(){
+     int k = 30;
+     int l = 40;
+     int m = 35;
+     int temp;
+     temp=k > l ? k : l;
+    System.out.println(temp > m ? temp :m);
+
+}
 
 
+@Test
+public void input(){
+        //new一个键盘输入对象，
+    Scanner sc = new Scanner(System.in);
+    //调用对象功能，等待输入
+    System.out.println("input age");
+    int age = sc.nextInt();
+    System.out.println("input name");
+    String name = sc.next();
+    System.out.println(name +age);
+}
+@Test
+public void mountain(){
+     double paper=0.1;
+     int count=0;
+     while (paper<8848860){
+         paper*=2;
+         count++;
+     }
+    System.out.println(count);
+    System.out.println(paper);
+}
 }
